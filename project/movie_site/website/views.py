43f73cc04ab11df898840cb26f39a586238d51f2
Leslie Nguyen <Leslie_d_n@hotmail.com>
2022-11-15 17:46:45 -0700

    customer can login and out now

--- project/movie_site/website/views.py
+++ project/movie_site/website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 from .forms import CreateUserForm
 
 # Create your views here.
@@ -23,8 +25,31 @@ def registerPage(request):
 				user = form.cleaned_data.get('username')
 				messages.success(request, 'Account was created for ' + user)
 
-				return redirect('home')
+				return redirect('login')
 			
 
 		context = {'form':form}
 		return render(request, 'register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
