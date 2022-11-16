eb17736f53fb09562d77c497d9485698b6a36f61
Leslie Nguyen <Leslie_d_n@hotmail.com>
2022-11-15 17:34:52 -0700

    custer registarion goes to other page and is in databse

--- project/movie_site/website/urls.py
+++ project/movie_site/website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.registerPage, name="register"),
+    path('test/', views.test, name='test')
 ]
--- project/movie_site/website/views.py
+++ project/movie_site/website/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect 
+from .forms import CreateUserForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def test(request):
+    return render(request, 'test.html', {})
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('home')
+			
+
+		context = {'form':form}
+		return render(request, 'register.html', context)
